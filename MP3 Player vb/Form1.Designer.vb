e9fa242d1caeab4c50a3c132d457e266e5fd3861
Jeewantha Bandara <51124088+JeewanthaBandara97@users.noreply.github.com>
2019-08-11 09:26:15 +0530

    Add files via upload

---
+++ MP3 Player vb/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.button5 = New System.Windows.Forms.Button()
+        Me.button4 = New System.Windows.Forms.Button()
+        Me.button3 = New System.Windows.Forms.Button()
+        Me.button2 = New System.Windows.Forms.Button()
+        Me.button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.AxWindowsMediaPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'button5
+        '
+        Me.button5.Location = New System.Drawing.Point(387, 1)
+        Me.button5.Name = "button5"
+        Me.button5.Size = New System.Drawing.Size(68, 30)
+        Me.button5.TabIndex = 13
+        Me.button5.Text = "Stop"
+        Me.button5.UseVisualStyleBackColor = True
+        '
+        'button4
+        '
+        Me.button4.Location = New System.Drawing.Point(299, 1)
+        Me.button4.Name = "button4"
+        Me.button4.Size = New System.Drawing.Size(82, 30)
+        Me.button4.TabIndex = 10
+        Me.button4.Text = "Resume"
+        Me.button4.UseVisualStyleBackColor = True
+        '
+        'button3
+        '
+        Me.button3.Location = New System.Drawing.Point(225, 1)
+        Me.button3.Name = "button3"
+        Me.button3.Size = New System.Drawing.Size(68, 30)
+        Me.button3.TabIndex = 9
+        Me.button3.Text = "Pause"
+        Me.button3.UseVisualStyleBackColor = True
+        '
+        'button2
+        '
+        Me.button2.Location = New System.Drawing.Point(154, 1)
+        Me.button2.Name = "button2"
+        Me.button2.Size = New System.Drawing.Size(65, 30)
+        Me.button2.TabIndex = 8
+        Me.button2.Text = "Play"
+        Me.button2.UseVisualStyleBackColor = True
+        '
+        'button1
+        '
+        Me.button1.Location = New System.Drawing.Point(1, 1)
+        Me.button1.Name = "button1"
+        Me.button1.Size = New System.Drawing.Size(65, 30)
+        Me.button1.TabIndex = 7
+        Me.button1.Text = "Open"
+        Me.button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'AxWindowsMediaPlayer1
+        '
+        Me.AxWindowsMediaPlayer1.Enabled = True
+        Me.AxWindowsMediaPlayer1.Location = New System.Drawing.Point(4, 37)
+        Me.AxWindowsMediaPlayer1.Name = "AxWindowsMediaPlayer1"
+        Me.AxWindowsMediaPlayer1.OcxState = CType(resources.GetObject("AxWindowsMediaPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.AxWindowsMediaPlayer1.Size = New System.Drawing.Size(336, 182)
+        Me.AxWindowsMediaPlayer1.TabIndex = 14
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(457, 272)
+        Me.Controls.Add(Me.AxWindowsMediaPlayer1)
+        Me.Controls.Add(Me.button5)
+        Me.Controls.Add(Me.button4)
+        Me.Controls.Add(Me.button3)
+        Me.Controls.Add(Me.button2)
+        Me.Controls.Add(Me.button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "MP3 Player"
+        CType(Me.AxWindowsMediaPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private WithEvents button5 As Button
+    Private WithEvents button4 As Button
+    Private WithEvents button3 As Button
+    Private WithEvents button2 As Button
+    Private WithEvents button1 As Button
+    Friend WithEvents AxWindowsMediaPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+End Class
---
+++ MP3 Player vb/Form1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Windows.Forms
+
+Public Class Form1
+    Private Sub button1_Click(sender As Object, e As EventArgs) Handles button1.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles button2.Click
+        AxWindowsMediaPlayer1.URL = OpenFileDialog1.FileName
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+    End Sub
+
+    Private Sub button4_Click(sender As Object, e As EventArgs) Handles button4.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.play()
+    End Sub
+
+    Private Sub button3_Click(sender As Object, e As EventArgs) Handles button3.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.pause()
+    End Sub
+
+    Private Sub button5_Click(sender As Object, e As EventArgs) Handles button5.Click
+        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+    End Sub
+
+    Private Sub trackBar1_Scroll(sender As Object, e As EventArgs)
+
+
+
+
+
+    End Sub
+End Class
